0fc6f5811220e4d3c7ecbc22025fa60610f951a8
frsaem00 <matthieu.saeleuve@ingrammicro.com>
2026-08-25 11:25:13 +0200

    Initialisation de la nouvelle branche avec fichier CS

---
+++ Gestion_Requete_FR/Gestion_Requete_FR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTION_REQUETE_FR
+{
+    public class GESTION_REQUETE_FR
+    {
+        public GESTION_REQUETE_FR() { 
+        }
+
+
+    }
+}
