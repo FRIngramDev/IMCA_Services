2bd9c2eb377d261d2534656dc6d99f618062ffd1
frsaem00 <matthieu.saeleuve@ingrammicro.com>
2026-08-31 16:12:40 +0200

    First commit : creation du repertoire et classe pour  IMEI_MANAGEMENT_FR

---
+++ Imei_Management_FR/Imei_Management_FR.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMEI_MANAGEMENT_FR
+{
+    public class IMEI_MANAGEMENT_FR
+    {
+        public IMEI_MANAGEMENT_FR() { 
+        } 
+    }
+}
